bf9b8ff2d110dbcf7e970f1e0f672b8764cd50cc
dev on AliYun <chinalihongde@163.com>
2015-09-01 17:36:24 +0800

    class.py modify,private variables

--- class.py
+++ class.py
@@ -9,3 +9,22 @@ class Student(object):
 
 aStudent = Student('sophia',22)
 aStudent.info()
+
+# 私有变量以__开头加变量名 __var
+class boy(object):
+    def __init__(self,name,age,sex):
+        self.name = name
+	self.__age = age
+	self.sex = sex
+    def self_desc(self):
+	print('My name is %s,a %s,%s years old.' %(self.name,self.sex,self.__age))
+
+someone = boy('kevin',22,'male');
+someone.self_desc();
+"""
+private_age = someone._boy__age
+print(private_age)
+"""
+# 以_开头的变量外部可以访问，但是其目的是标示出来，建议不在外部访问
+# 以双划线开头并以双划线结尾的变更是特殊变量
+# 以又划线开头的变量其实仍可访问，但是不建议 someone._boy__age
