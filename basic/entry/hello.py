3c5635b7b4fccd400f25844c193bafcd1283b397
root <root@localhost.localdomain>
2016-04-20 18:45:50 +0800

    module __name__ __main__

---
+++ basic/entry/hello.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+# -*- coding:utf-8 -*-
+def hello():
+	str = "hello every buddies!"
+	print(str)
+	
+if __name__ == "__main__":
+	print("call hello.py as a main module.")
+	hello()
